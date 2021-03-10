075a29d867d235f4012a46b8a66890a8b4306a73
Marquise Piton <pitonmarquise@gmail.com>
2021-03-10 15:11:11 -0500

    6th commit

--- js/main.js
+++ js/main.js
@@ -3,87 +3,98 @@ const title = document.getElementById('title');
 const directions = document.getElementById('directions');
 const sig = document.getElementById('sig');
 
-const playerOneMaker = document.getElementById('playerOneMaker');
-const playerTwoMaker = document.getElementById('playerTwoMaker');
 
 
-start.addEventListener('click',hideMe);
 
 
-function hideMe(){
 
-    start.style.visibility = "hidden";
-    title.style.visibility = "hidden";
-    directions.style.visibility = "hidden";
-    sig.style.visibility = "hidden";
 
-}
-
- 
-class Player{
-    constructor(name,score,symbol){
+class Player {
+    constructor(name, score, symbol) {
         this.name = name;
         this.score = score;
         this.symbol = symbol;
     }
+
+     playerName(string){
+
+        this.name = string;
+    }
+
 }
 
 
 class Game {
-    constructor(){
-        this.player1 = new Player("Player 1",0,"X");
-        this.player2 = new Player("Player 2",0,"O");
+    constructor() {
+        this.player1 = new Player("Player 1", 0, "X");
+        this.player2 = new Player("Player 2", 0, "O");
     }
 
-    init(){
-        let instructions = ['Player 1 type your name:','Player 2 type your name:'];
+    init() {
+
+        let instructions = ['Player 1 type your name:', 'Player 2 type your name:'];
         let textInputBoxPlayerOne = document.createElement('input');
+        textInputBoxPlayerOne.appendChild(document.createElement('button'));
         let textInputBoxPlayerTwo = document.createElement('input');
 
 
 
         // Getting the name for player one
-       
-        let playerOneName = document.getElementById('playerOneInput').appendChild(textInputBoxPlayerOne);
-        let p1 =  playerOneInstruct.innerHTML = instructions[0];  
-        
-        
 
-    
+        document.getElementById('playerOneInput').appendChild(textInputBoxPlayerOne);
+        let p1 = playerOneInstruct.innerHTML = instructions[0];
 
         // Getting the name for player two
+
+        document.getElementById('playerTwoInput').appendChild(textInputBoxPlayerTwo);
+        let p2 = playerTwoInstruct.innerHTML = instructions[1];
+
+
+
+        // let arrayOfPlayer = [p1,p2];
+
+
+        // for(let i = 0; i <= arrayOfPlayer.length; i++){
+        //     arrayOfPlayer[i];
+
+        // }
+
+
+        start.addEventListener('click', () =>{
+
+            this.player1.playerName(textInputBoxPlayerOne.value);
+            this.player2.playerName(textInputBoxPlayerTwo.value);
+            this.generateBoard();
+
+            console.log(this.player1);
+            console.log(this.player2);
+
+        } );
+
         
-        let playerTwoName = document.getElementById('playerTwoInput').appendChild(textInputBoxPlayerTwo);
-        let p2  = PlayerTwoInstruct.innerHTML = instructions[1];
 
-
-
-        let arrayOfPlayer = [p1,p2];
-
-
-        for(let i = 0; i <= arrayOfPlayer.length; i++){
-            arrayOfPlayer[i];
-        }
-
-
-
-
-
-        this.player1 = playerOneName.value;
-        this.player2 = playerTwoName.value;
-
-
-
-
-        generateBoard();
-        
 
     }
 
     // Creates the tiles
-    generateBoard(){
+    generateBoard() {
 
-        for(let i = 0; i <=9; i++){
+        playerOneInstruct.style.visibility = 'hidden';
+        playerTwoInstruct.style.visibility = 'hidden';
+        playerOneInput.style.visibility = 'hidden';
+        playerTwoInput.style.visibility = 'hidden';
+        start.style.visibility = "hidden";
+        title.style.visibility = "hidden"; 
+        directions.style.visibility = "hidden";
+        sig.style.visibility = "hidden";
+
+        let grid = document.createElement('div');
+            grid.setAttribute("class",'grid');
+
+        for (let i = 0; i < 9; i++) {
+
+           
+           grid.appendChild();
 
         }
 
@@ -121,13 +132,13 @@ class Game {
 
 class App {
 
-    constructor(){
+    constructor() {
         this.game = new Game();
     }
     init() {
         console.log("starting the app");
         this.game.init();
-        
+
 
     }
 }
